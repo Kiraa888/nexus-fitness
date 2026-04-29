5f65c769c16e7e5d404d99e871f06834a05bd28a
Animesh Bauri <159995350+Kiraa888@users.noreply.github.com>
2026-04-29 13:20:38 +0530

    Create exercises.js

---
+++ js/data/exercises.js
@@ -0,0 +1,7 @@
+export const exerciseDB = [
+    { id: 'e1', name: 'Bench Press', muscleGroup: 'chest' },
+    { id: 'e2', name: 'Barbell Squat', muscleGroup: 'legs' },
+    { id: 'e3', name: 'Deadlift', muscleGroup: 'back' },
+    { id: 'e4', name: 'Pull-Up', muscleGroup: 'back' },
+    { id: 'e5', name: 'Overhead Press', muscleGroup: 'shoulders' }
+];
